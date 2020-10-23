449a6461606737db27c8e43c7a11bd285ded54d1
Greg <gregory_reinhardt@student.uml.edu>
2020-10-23 08:38:00 -0400

    TreeMatchGR.py: TreeMatch didn't get pushed last time I don't think. Trying again.

--- TreeMatchGR.py
+++ TreeMatchGR.py
@@ -13,16 +13,29 @@ import sys
 import random
 
 
-frame = cv2.imread("Resources\\TestImages\\Tree1.png")
+frame = cv2.imread("Resources\\TestImages\\GrassArrow.png")
 frame = cv2.cvtColor(frame,cv2.COLOR_RGBA2RGB)
 
-outline_mask = cv2.inRange(frame, (1,1,1), (15,15,15))
+# for i in range(0,10):                             # Pixel location top left of box
+#     for j in range(0, 10):
+#         frame[289+i,491+j] = [255, 255, 255]
+
+
+
+outline_mask = cv2.inRange(frame, (1,1,1), (30,30,30))      # Issue with words above character (outlined)
 tree_mask = cv2.inRange(frame, (0,10,30), (3,30,60))
+arrow_mask = cv2.inRange(frame, (34,100,164), (34,100,164))
+edges = cv2.Canny(frame, 50, 450)
 
 while (True):
+
+    cv2.imshow('edges',edges)
+    cv2.imshow('Arrows', arrow_mask)
     cv2.imshow('frame', frame)
     cv2.imshow('Stumps!', tree_mask)
     cv2.imshow('Outlines!', outline_mask)
+
+    time.sleep(.5)
 
     if cv2.waitKey(25) & 0xFF == ord("q"):
         cv2.destroyAllWindows()
