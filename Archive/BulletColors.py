796a0e0c6a368cdd589687566e6335e0d48a5b32
Greg <gregory_reinhardt@student.uml.edu>
2020-11-06 16:54:13 -0500

    BulletTrackingTest.py: Did some testing on bullet tracking. I think color filtering is the way to go once again. Seems to work well and the minAreaRec function returns an angle of rotation which should be helpful. Still need to figure out how to handle our own bullets. Pulled bullet images from wiki and ran BulletColors.py in Archive to get unique colors in bullets.
    BulletColors.py:
    In Archive folder. Returns a list of colors with pixel counts. Currently points to bullet directory but could be used to read in any images. Might be useful for other things.

---
+++ Archive/BulletColors.py
@@ -0,0 +1,57 @@
+import cv2
+import os
+import time
+import numpy as np
+import matplotlib
+
+colorList = []
+
+for subdir, dirs, files in os.walk("../Resources/Bullets"):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+
+        if filepath.endswith(".jpg") or filepath.endswith(".png"):
+            print(filepath)
+
+        img = cv2.imread(filepath)
+
+        for row in img:
+            for pixel in row:
+                    inpL = False
+                    if list(pixel) != [84, 84, 84]:
+                        for p in colorList:
+                            if list(pixel) == p[0] and filename == p[1]:
+                                p[2] += 1
+                                inpL = True
+
+                        if not inpL:
+                            colorList.append([list(pixel),filename,1])
+
+        # cv2.imshow("Image",img)
+        #
+        # cv2.waitKey(1)
+        #
+        # time.sleep(1)
+
+
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for color in colorList:
+    print(color[0]," ",color[1]," ",color[2])
+    xs = color[0][0]
+    ys = color[0][1]
+    zs = color[0][2]
+    ax.scatter(xs=xs, ys=ys, zs=zs,c=[(color[0][2]/255,color[0][1]/255,color[0][0]/255)])
+
+
+ax.set_xlabel('B')
+ax.set_ylabel('G')
+ax.set_zlabel('R')
+
+plt.show()
---
+++ BulletTrackingTest.py
@@ -0,0 +1,149 @@
+import cv2
+import time
+import numpy as np
+import GrabScreen
+import Utils
+
+bulletColors = [
+[134, 171, 43],
+[238, 255, 196],
+[230, 230, 230],
+[255, 255, 255],
+[66, 66, 66],
+[16, 16, 16],
+[143, 143, 143],
+[112, 112, 112],
+[44, 45, 45],
+[44, 45, 45],
+[181, 181, 181],
+[181, 181, 181],
+[211, 211, 211],
+[255, 255, 255],
+[227, 99, 64],
+[255, 155, 128],
+[227, 99, 64],
+[255, 155, 128],
+[255, 111, 90],
+[248, 175, 162],
+[255, 111, 90],
+[248, 175, 162],
+[168, 73, 59],
+[255, 112, 90],
+[248, 176, 162],
+[24, 57, 80],
+[38, 90, 126],
+[166, 156, 0],
+[239, 227, 37],
+[166, 39, 0],
+[239, 84, 37],
+[53, 53, 53],
+[30, 30, 30],
+[102, 102, 102],
+[127, 127, 127],
+[117, 117, 117],
+[71, 71, 71],
+[0, 0, 255],
+[0, 212, 255],
+[98, 249, 255],
+[193, 252, 255],
+[12, 238, 255],
+[38, 218, 255],
+[97, 247, 255],
+[25, 201, 255],
+[175, 175, 175],
+[206, 206, 206],
+[232, 232, 232],
+[247, 247, 247],
+[255, 255, 255],
+[254, 254, 254],
+[34, 100, 164],
+[39, 206, 163],
+[26, 137, 68],
+[157, 157, 157],
+[64, 166, 0],
+[114, 240, 36],
+[41, 148, 0],
+[65, 229, 0],
+[189, 255, 183],
+[160, 160, 160],
+[243, 243, 243],
+[181, 181, 181],
+[255, 255, 255],
+[255, 100, 89],
+[255, 148, 130],
+[255, 183, 184],
+[255, 144, 142],
+[255, 174, 173],
+[0, 142, 255],
+[153, 210, 255],
+[255, 66, 223],
+[255, 217, 249],
+[255, 66, 223],
+[255, 217, 249],
+[135, 34, 116],
+[255, 65, 220],
+[255, 217, 248],
+[29, 29, 222],
+[56, 63, 238],
+[0, 0, 181],
+[53, 38, 190],
+[0, 0, 255],
+[0, 212, 255],
+[0, 0, 124],
+[36, 28, 240],
+[66, 66, 255],
+[177, 177, 255],
+[177, 13, 113],
+[255, 136, 208],
+[237, 202, 202],
+[255, 217, 179],
+[255, 237, 219],
+[255, 255, 255],
+[255, 247, 247],
+[0, 212, 255],
+[179, 236, 246]
+]
+
+gameWindow = GrabScreen.findWindow("RotMGExalt")
+
+while(True):
+
+    frame = GrabScreen.captureScreen(gameWindow)
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
+    frame = Utils.cutGameFrame(frame)
+
+    start_time = time.time()
+
+    # Create blank image for concatenating masks
+    x, y, c = frame.shape
+    result = np.zeros((x, y), dtype="uint8")
+
+    # Add mask for each color
+    for color in bulletColors:
+        mask = cv2.inRange(frame, (color[0], color[1], color[2]), (color[0], color[1], color[2]))
+        result = cv2.add(result, mask)
+
+    # Dilate makes all the mask areas thicker so they group better
+    kernel = np.ones((2, 2), np.uint8)
+    result = cv2.dilate(result, kernel, iterations=1)
+
+    # Get contours of masks
+    contours, hierarchy = cv2.findContours(result, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    # Get bounding boxes of contours and draw them
+    for c in contours:
+        #area = cv2.contourArea(c)
+        rect = cv2.minAreaRect(c)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.drawContours(frame, [box], 0, (0, 0, 255), 1)
+
+    # To test amount of time required to process... .08sec for 81 colors, .03sec for 36 on my machine
+    print(time.time() - start_time)
+
+    # Show frame
+    cv2.imshow("Bullet Tracking", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        cv2.destroyAllWindows()
+        break
